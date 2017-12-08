59e8ab61ca695903803bb6ac6d3af238cb3a03f0
Dylan Martens <dylanhs123@hotmail.com>
2017-12-08 09:17:45 +0100

    Added scroll to top button

---
+++ js/backtotopbutton.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+     $(window).scroll(function () {
+            if ($(this).scrollTop() > 500) {
+                $('#back-to-top').fadeIn();
+            } else {
+                $('#back-to-top').fadeOut();
+            }
+        });
+        // scroll body to 0px on click
+        $('#back-to-top').click(function () {
+            $('#back-to-top').tooltip('hide');
+            $('body,html').animate({
+                scrollTop: 0
+            }, 1000, "easeInOutExpo");
+            return false;
+        });
+});
